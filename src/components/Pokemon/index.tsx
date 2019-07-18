87cd304bc5ae00d722e10e844f5d0bd7bf11aad1
Nana Adjei Manu <claeusdev@outlook.com>
2019-07-18 01:21:57 +0000

    ui changes

--- src/components/Pokemon/index.tsx
+++ src/components/Pokemon/index.tsx
@@ -6,7 +6,7 @@ interface PokemonProps {
 }
 
 export function PokeMon({ pokemon }: PokemonProps) {
-    return <article className='tc pv4 pv5-ns'>
+    return <article className=' pv4 pv5-ns'>
         <img src={pokemon.imageUrl} className='br-100 pa1 ba b--black-10 h3 w3' alt='avatar' />
         <h1 className='f5 f4-ns fw6 mid-gray'>{pokemon.name}</h1>
         <h2 className='f6 gray fw2 ttu tracked'>Experience: {pokemon.baseExperience} / Abilities: {pokemon.numOfAbilities}</h2>
